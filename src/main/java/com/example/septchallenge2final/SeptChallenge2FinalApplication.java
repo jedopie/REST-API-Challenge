27f820614eb0b6be234ee6b142dc52c6fae41632
Jed Opie <jed.opie@hotmail.com>
2022-10-10 23:16:51 +1100

    Initial commit

---
+++ src/main/java/com/example/septchallenge2final/SeptChallenge2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge2final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptChallenge2FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptChallenge2FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/septchallenge2final/SeptChallenge2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge2final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallenge2FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
